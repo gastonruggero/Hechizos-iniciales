6b9dc6845681ce931f86e6a46ad1ab00d411dae7
aux user .2 <>
2024-04-09 18:04:45 -0300

    ej1 do while agregado

---
+++ Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
@@ -0,0 +1,26 @@
+const leer = require("prompt-sync")();
+
+/**
+ * Ej 1
+ * Te embarcas en una misión para destruir los Horrocruxes, objetos malditos que contienen fragmentos del alma del maligno mago Lord Voldemort. La destrucción de los Horrocruxes es crucial para derrotar a Voldemort y restaurar la paz en el mundo mágico.
+
+El estudiante debe enfrentarse a la tarea desafiante de encontrar y destruir cada uno de los Horrocruxes, los cuales están escondidos en diferentes lugares y protegidos por hechizos y encantamientos poderosos. Cada Horrocrux tiene su propia forma única de ser destruido, representada por un código secreto generado aleatoriamente.
+
+Diario de Tom Riddle        es "R1dd13" 
+Anillo de Gaunt             es "G4unt!"
+Copa de Helga Hufflepuff    es "H3lga!"
+Diadema de Rowena Ravenclaw es "R4vena"
+Nagini (serpiente)          es "N@9ini"
+
+Además de la búsqueda y la resolución de enigmas, el estudiante también debe administrar cuidadosamente sus puntos de cordura y salud. Los encuentros con los Horrocruxes y los contraataques de estos objetos malignos pueden afectar negativamente tanto la cordura como la salud del estudiante. Si los puntos de cordura o salud llegan a cero, el estudiante perderá el juego.
+
+El objetivo final del estudiante es destruir todos los Horrocruxes antes de que su cordura o salud se agoten por completo, lo que resultaría en su derrota. Al destruir todos los Horrocruxes, el estudiante habrá triunfado sobre las fuerzas oscuras y habrá ayudado a derrotar a Voldemort, restaurando la paz en el mundo mágico.
+ */
+
+
+function main() {
+    
+}
+
+
+main();
