3a0061fd04898c1b6515c1a9e0d2bbed8bb3639d
Erick <erick.nahuelpaiva.001@gmail.com>
2024-04-13 18:36:43 -0300

    terminado

--- Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
+++ Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
@@ -1,26 +1,5 @@
 const leer = require("prompt-sync")();
 
-/**
- * Ej 1
- * Te embarcas en una misión para destruir los Horrocruxes, objetos malditos que contienen fragmentos del alma del maligno mago Lord Voldemort. La destrucción de los Horrocruxes es crucial para derrotar a Voldemort y restaurar la paz en el mundo mágico.
-
-El estudiante debe enfrentarse a la tarea desafiante de encontrar y destruir cada uno de los Horrocruxes, los cuales están escondidos en diferentes lugares y protegidos por hechizos y encantamientos poderosos. Cada Horrocrux tiene su propia forma única de ser destruido, representada por un código secreto generado aleatoriamente.
-
-Diario de Tom Riddle        es "R1dd13?" donde ? es un numero generado aleatoriamente entre -3 y 20 
-Anillo de Gaunt             es "?G4unt!" donde ? es un numero generado aleatoriamente entre -100 y -70
-Copa de Helga Hufflepuff    es "H3l?ga!" donde ? es un numero generado aleatoriamente entre 4 y 12
-Diadema de Rowena Ravenclaw es "?R4vena" donde ? es un caracter generado aleatoriamente entre a y c 
-Nagini (serpiente)          es "N@9ini?" donde ? es un caracter generado aleatoriamente entre H y K 
-
-El estudiante tiene con 40 intentos para destruir todos, si es que aún sigue en pie
-
-Además de la búsqueda y la resolución de enigmas, el estudiante también debe administrar cuidadosamente sus puntos de cordura 200 y salud 400. Los encuentros con los Horrocruxes y los contraataques de estos objetos malignos pueden afectar negativamente tanto la cordura como la salud del estudiante. Si los puntos de cordura o salud llegan a cero, el estudiante perderá el juego.
-El daño de los Horrocruxes es igual a 5.7 aplicado a la cordura y 10.4 a la salud por cada intento fallido, por ejemplo si es el intento 11 el daño recibido hasta entonces es de 57
-
-El objetivo final del estudiante es destruir todos los Horrocruxes antes de que su cordura o salud se agoten por completo, lo que resultaría en su derrota. Al destruir todos los Horrocruxes, el estudiante habrá triunfado sobre las fuerzas oscuras y habrá ayudado a derrotar a Voldemort, restaurando la paz en el mundo mágico.
- */
-
-
 function main() {
     
 }
