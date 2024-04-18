62042a00c853481d5f8fb3059d881900a22278ae
pc 5 <>
2024-04-18 12:12:52 -0300

    ej2 DO WHILE COMENZADO

--- Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
+++ Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
@@ -1,4 +1,11 @@
 const leer = require("prompt-sync")();
+const DANIO_HORRO=.005;
+const INCREMENTE_DANIO_HORRO=0.0024;
+const INTENTOS_ESTUDIANTE=30;
+const PUNTOS_CORDURA_MAX=200;
+const PUNTOS_SALUD_MAX=400;
+const DANIO_CORDURA=.057;
+const DANIO_SALUD=.104;
 
 function main() {
     
