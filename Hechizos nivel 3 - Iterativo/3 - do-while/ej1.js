ada5954ab6dd7bf4157ece73335680aa97ed69a9
Gastón Ruggero <gastonruggero@gmail.com>
2024-04-23 01:08:03 -0300

    ej1 sin resolver!

--- Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
+++ Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
@@ -15,109 +15,102 @@ const CANTIDAD_HORRO_INICIAL = 5;
 
 const ID_DIARIO_TR = 1;
 const ID_ANILLO_G = 2;
-const ID_DIADEMA_RR = 4;
 const ID_COPA_HH = 3;
+const ID_DIADEMA_RR = 4;
 const ID_NAGINI = 5;
 
-let vida = true;
+
 let vidaNagini = true;
 let vidaDiademaRR = true;
 let vidaAnilloG = true;
 let vidaCopaHH = true;
 let vidaDiarioTR = true;
 
-/* function estaVivoHorrocrux(horrocrux, vida) {
-    if (!vida && horrocrux<=4 && horrocrux>=1){
-        horrocrux=horrocrux+1;
-        
-    } else if (!vida && horrocrux==5) {
-        horrocrux=horrocrux-1;        
-    } else {
-        console.log(`no quedan mas horrocruxes vivos`);
-    }
-   
-} */
+function comprobarVidaHorro(horrocrux) {
+    let horrocruxVivo = 0;
+    
+    if (vidaDiarioTR == true) {
+        horrocruxVivo = horrocrux ;
 
-function todosMuertos(){
+    } else {
+        horrocruxVivo = horrocrux + 1;
+    } 
+
+    if (vidaAnilloG ==true) {
+        horrocruxVivo = horrocrux;
+    }else {
+        horrocruxVivo = horrocrux +1;
+    }
+    if (vidaDiademaRR == true) {
+        horrocruxVivo = horrocrux;
+    }else {
+        horrocruxVivo = horrocrux + 1;
+    }
+     if (vidaCopaHH == true) {
+        horrocruxVivo = horrocrux;
+    } else {
+        horrocruxVivo = horrocrux +1;
+    }
+     if (vidaNagini ==true) {
+        horrocruxVivo = horrocrux;
+    } else {
+        horrocruxVivo = 1;
+    }
+    return horrocruxVivo;
+}
+
+function todosMuertos() {
     !vidaAnilloG && !vidaCopaHH && !vidaDiademaRR && !vidaDiarioTR && !vidaNagini;
 }
 
-function generarHorrocrux(horrocrux) {
+function generarHorrocrux(horrocruxVivo) {
     let llave = "";
     let pase = "";
-    
-        switch (horrocrux) {
 
-            case ID_DIARIO_TR:
-                if (vidaDiarioTR) {
-                    console.log("*** Te enfrentarás al Diario de Tom Riddle.\nLa contraseña es", DIARIO_TR, "donde ? es un numero generado aleatoriamente entre -3 y 20.");
-                    llave = String((Math.floor(Math.random() * 23)) - 3);
-                    pase = DIARIO_TR.replace("?", llave);
-                    console.log("La contraseña correcta es:", pase);
-                } else {
-                    horrocrux = horrocrux + 1;
-                    generarHorrocrux(horrocrux);
-                }
+    switch (horrocruxVivo) {
 
+        case ID_DIARIO_TR:
+            console.log("*** Te enfrentarás al Diario de Tom Riddle.\nLa contraseña es", DIARIO_TR, "donde ? es un numero generado aleatoriamente entre -3 y 20.");
+            llave = String((Math.floor(Math.random() * 23)) - 3);
+            pase = DIARIO_TR.replace("?", llave);
+            console.log("La contraseña correcta es:", pase);
+            break;
+        case ID_ANILLO_G:
 
-                break;
-            case ID_ANILLO_G:
-                if (vidaAnilloG) {
-                    console.log("*** Te enfrentarás al Anillo de Gaunt.\nLa contraseña es", ANILLO_G, "donde ? es un numero generado aleatoriamente entre -100 y -70.");
-                    llave = String((Math.floor(Math.random() * 30)) - 100);
-                    pase = ANILLO_G.replace("?", llave);
-                    console.log("La contraseña correcta es:", pase);
-                } else {
-                    horrocrux = horrocrux + 1;
-                    generarHorrocrux(horrocrux);
-                }
-                break;
-            case ID_COPA_HH:
-                if (vidaCopaHH) {
-                    console.log("*** Te enfrentarás a la Copa de Helga Hufflepuff.\nLa contraseña es", COPA_HH, "donde ? es un numero generado aleatoriamente entre 4 y 12.");
-                    llave = String((Math.floor(Math.random() * 8)) + 4);
-                    pase = COPA_HH.replace("?", llave);
-                    console.log("La contraseña correcta es:", pase);
-                } else {
-                    horrocrux = horrocrux + 1;
-                    generarHorrocrux(horrocrux);
-                }
-                break;
-            case ID_DIADEMA_RR:
-                if (vidaDiademaRR) {
-                    console.log("*** Te enfrentarás a la Diadema de Rowena Ravenclaw.\nLa contraseña es", DIADEMA_RR, "donde ? es un caracter generado aleatoriamente entre a y e.");
-                    llave = String.fromCharCode((Math.floor(Math.random() * 5)) + 97);
-                    pase = DIADEMA_RR.replace("?", llave);
-                    console.log("La contraseña correcta es:", pase);
-                } else {
-                    horrocrux = horrocrux + 1;
-                    generarHorrocrux(horrocrux);
-                }
-
-                break;
-            case ID_NAGINI:
-                if (vidaNagini) {
-                    console.log("*** Te enfrentarás a la Nagini.\nLa contraseña es", NAGINI, "donde ? es un caracter generado aleatoriamente entre H y K.");
-                    llave = String.fromCharCode((Math.floor(Math.random() * 4)) + 72);
-                    pase = NAGINI.replace("?", llave);
-                    console.log("La contraseña correcta es:", pase);
-                } else {
-                    horrocrux = ID_DIARIO_TR;
-                    generarHorrocrux(horrocrux);
-                }
-
-                break;
-            default:
-                console.log("otra opcion");
-                break;
-        }
-
-        return llave;
+            console.log("*** Te enfrentarás al Anillo de Gaunt.\nLa contraseña es", ANILLO_G, "donde ? es un numero generado aleatoriamente entre -100 y -70.");
+            llave = String((Math.floor(Math.random() * 30)) - 100);
+            pase = ANILLO_G.replace("?", llave);
+            console.log("La contraseña correcta es:", pase);
+            break;
+        case ID_COPA_HH:
+            console.log("*** Te enfrentarás a la Copa de Helga Hufflepuff.\nLa contraseña es", COPA_HH, "donde ? es un numero generado aleatoriamente entre 4 y 12.");
+            llave = String((Math.floor(Math.random() * 8)) + 4);
+            pase = COPA_HH.replace("?", llave);
+            console.log("La contraseña correcta es:", pase);
+            break;
+        case ID_DIADEMA_RR:
+            console.log("*** Te enfrentarás a la Diadema de Rowena Ravenclaw.\nLa contraseña es", DIADEMA_RR, "donde ? es un caracter generado aleatoriamente entre a y e.");
+            llave = String.fromCharCode((Math.floor(Math.random() * 5)) + 97);
+            pase = DIADEMA_RR.replace("?", llave);
+            console.log("La contraseña correcta es:", pase);
+            break;
+        case ID_NAGINI:
+            console.log("*** Te enfrentarás a la Nagini.\nLa contraseña es", NAGINI, "donde ? es un caracter generado aleatoriamente entre H y K.");
+            llave = String.fromCharCode((Math.floor(Math.random() * 4)) + 72);
+            pase = NAGINI.replace("?", llave);
+            console.log("La contraseña correcta es:", pase);
+            break;
+        default:
+            console.log("otra opcion");
+            break;
     }
 
-function muerteHorrocrux(horrocrux) {
+    return llave;
+}
 
-    switch (horrocrux) {
+function muerteHorrocrux(horrocruxVivo) {
+
+    switch (horrocruxVivo) {
         case ID_DIARIO_TR:
             console.log("Acabaste con el Diario de TR");
             vidaDiarioTR = false;
@@ -140,6 +133,7 @@ function muerteHorrocrux(horrocrux) {
             break;
 
     }
+   /* horrocruxVivo=0;*/
 
 }
 
@@ -153,21 +147,25 @@ function main() {
     let intento = 1;
     let danioTotalGen = 0;
     let danioAcumulado = PROB_DANIO_HORRO;
-    console.log("*** Hoy vas a luchar contra los Horrocruxes");
-    do {
+    let horrocruxVivo = 0;
 
+    console.log("*** Hoy vas a luchar contra los Horrocruxes");
+    for (intento = 1; intento <= 10; intento++) {
         console.log("### Salud: ", salud, "### Cordura: ", cordura, intento);
-        horrocrux = Math.floor(Math.random() * CANTIDAD_HORRO_INICIAL) + 1;
+        console.log(vidaAnilloG, ID_ANILLO_G, vidaCopaHH, ID_COPA_HH, vidaDiademaRR, ID_DIADEMA_RR, vidaDiarioTR, ID_DIARIO_TR, vidaNagini, ID_NAGINI);
+        horrocrux = Math.floor(Math.random() * CANTIDAD_HORRO_INICIAL)+1;
         console.log(horrocrux);
-        llave = generarHorrocrux(horrocrux);
+        horrocruxVivo = comprobarVidaHorro(horrocrux);
+        console.log(horrocruxVivo);
+        llave = generarHorrocrux(horrocruxVivo);
         ingresoUsuario = String(leer());
         if (ingresoUsuario === llave) {
             console.log("correcto");
             console.log("Has acertado y destruido al Horrocrux. Atención! Su capacidad de daño aumentó!");
             danioAcumulado = PROB_DANIO_HORRO + INCREMENTO_PROB_DANIO_HORRO;
-            muerteHorrocrux(horrocrux);
-          
-            intento = intento + 1;
+            muerteHorrocrux(horrocruxVivo);
+
+            
         } else {
             console.log("fallaste");
             danioTotalGen = Math.random();
@@ -183,15 +181,16 @@ function main() {
                 cordura = cordura - (cordura * DANIO_CORDURA);
                 intento = intento + 1;
             }
+
         }
 
-
-
-
-
     }
-    while (intento <= 10 && salud >= 1 && cordura >= 1 || todosMuertos());
-    console.log("Juego finalizado.");
+
+
+
+
+
+
 
 }
 main();
