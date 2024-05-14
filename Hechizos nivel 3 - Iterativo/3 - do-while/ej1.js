1d6a66452b0ae2d7e67ebafd614267710211f483
pc 5 <>
2024-05-14 12:21:36 -0300

    salud o cordura?

--- Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
+++ Hechizos nivel 3 - Iterativo/3 - do-while/ej1.js
@@ -14,7 +14,7 @@ const ANILLO_G = "?G4unt!";
 const COPA_HH = "H3l?ga!";
 const DIADEMA_RR = "?R4vena";
 const NAGINI = "N@9ini?";
-const LLAVE_FIN = 25;
+const LLAVE_FINAL = "umba";
 const MAX_INTENTOS = 30;
 const ID_DIARIO_TR = 1;
 const ID_ANILLO_G = 2;
@@ -25,6 +25,7 @@ const MENSAJE_DERROTA = "- En el oscuro manto de la derrota, el estudiante enfre
 const MENSAJE_VICTORIA = "- ¡Victoria para el estudiante valiente que, con coraje y determinación, ha destruido todos los horrocruxes! Con cada fragmento de alma oscura eliminado, la luz de la esperanza ha brillado más brillante sobre el mundo mágico. ¡Su sacrificio y valentía han salvado a nuestra comunidad de las sombras de la oscuridad, asegurando un futuro lleno de paz y prosperidad para todas las generaciones venideras!"
 let salud = PUNTOS_SALUD_MAX;
 let cordura = PUNTOS_CORDURA_MAX;
+let danioAcumulado = PROB_DANIO_HORRO;
 /**
  * Genera un Horrocrux para luchar contra él.
  * @param {Number} horrocruxActual que aparece frente nuestro para luchar.
@@ -32,36 +33,31 @@ let cordura = PUNTOS_CORDURA_MAX;
  */
 function generarHorrocrux(horrocruxActual) {
     let llave = "";
-    let pase = "";
     switch (horrocruxActual) {
         case ID_DIARIO_TR:
             console.log("*** Te enfrentarás al Diario de Tom Riddle.\nLa contraseña es", DIARIO_TR, "donde ? es un numero generado aleatoriamente entre -3 y 20.");
             llave = String((Math.floor(Math.random() * 23)) - 3);
-            pase = DIARIO_TR.replace("?", llave);
             break;
         case ID_ANILLO_G:
             console.log("*** Te enfrentarás al Anillo de Gaunt.\nLa contraseña es", ANILLO_G, "donde ? es un numero generado aleatoriamente entre -100 y -70.");
             llave = String((Math.floor(Math.random() * 30)) - 100);
-            pase = ANILLO_G.replace("?", llave);
             break;
         case ID_COPA_HH:
             console.log("*** Te enfrentarás a la Copa de Helga Hufflepuff.\nLa contraseña es", COPA_HH, "donde ? es un numero generado aleatoriamente entre 4 y 12.");
             llave = String((Math.floor(Math.random() * 8)) + 4);
-            pase = COPA_HH.replace("?", llave);
             break;
         case ID_DIADEMA_RR:
             console.log("*** Te enfrentarás a la Diadema de Rowena Ravenclaw.\nLa contraseña es", DIADEMA_RR, "donde ? es un caracter generado aleatoriamente entre a y e.");
             llave = String.fromCharCode((Math.floor(Math.random() * 5)) + 97);
-            pase = DIADEMA_RR.replace("?", llave);
             break;
         case ID_NAGINI:
             console.log("*** Te enfrentarás a Nagini.\nLa contraseña es", NAGINI, "donde ? es un caracter generado aleatoriamente entre H y K.");
             llave = String.fromCharCode((Math.floor(Math.random() * 4)) + 72);
-            pase = NAGINI.replace("?", llave);
             break;
         default:
             victoria();
-            llave = LLAVE_FIN;
+            console.log("sale default");
+            llave = LLAVE_FINAL;
             break;
     }
     return llave;
@@ -88,6 +84,7 @@ function muerteHorrocrux(horrocruxActual) {
             break;
         case ID_NAGINI:
             console.log("Acabaste con Nagini");
+            victoria();
             break;
     }
     return todosMuertos;
@@ -101,7 +98,6 @@ function muerteHorrocrux(horrocruxActual) {
 function generarDialogo(intento, horrocruxActual) {
     let ingresoUsuario = "";
     console.log("### Salud: ", salud, "### Cordura: ", cordura, "### Intento: ", intento);
-    console.log(horrocruxActual);
     ingresoUsuario = String(leer());
     return ingresoUsuario;
 }
@@ -136,22 +132,43 @@ function ingresoErrado(esquivarDanioAcumulado, danioAcumulado, intento) {
     console.log("fallaste");
     danioTotalGen = Math.random();
     probEsquivarGen = Math.random();
+    console.log(probEsquivarGen);
     if (probEsquivarGen < esquivarDanioAcumulado) {
         salud = salud - (salud * DANIO_SALUD);
         cordura = cordura - (cordura * DANIO_CORDURA);
         intento = intento + 1;
-    } else{
-        console.log("Has logrado esquivar el ataque del Horrocrux!");
+    } else {
+        elegirPorIngresoErrado();
     }
     if (danioTotalGen < danioAcumulado) {
         cordura = 0;
         salud = 0;
         intento = MAX_INTENTOS;
         derrota();
-    }
-    console.log("daño", danioTotalGen, danioAcumulado);
+    }   
 
     return intento;
+}
+function elegirPorIngresoErrado(){
+    let opcion="";
+    console.log("Deseas proteger tu cordura [C] o tu salud [S]?");
+    opcion=leer();
+    switch (opcion) {
+        case "C":
+            console.log("Elegiste cuidar tu cordura");
+            salud=salud-(salud*0.104);            
+            break;
+            case "S":
+                console.log("Elegiste cuidar tu salud");
+                cordura=cordura-(cordura*0.057);
+                break;
+        default:
+            console.log("Opción equivocada, perdiste tu chance");
+            salud=salud-(salud*0.104);
+            cordura=cordura-(cordura*0.057);
+            break;
+    } 
+    
 }
 /**
  * Función principal del juego
@@ -162,12 +179,13 @@ function main() {
     let llave = "";
     let ingresoUsuario = "";
     let intento = 1;
-    let danioAcumulado = PROB_DANIO_HORRO;
+
     let horrocruxActual = 1;
     let todosMuertos = false;
     console.log("*** Hoy vas a luchar contra los Horrocruxes");
     do {
         llave = generarHorrocrux(horrocruxActual);
+        console.log(llave);//muestro el caracter faltante para prueba
         ingresoUsuario = generarDialogo(intento, horrocruxActual);
         if (ingresoUsuario === llave) {
             ingresoOK();
@@ -181,6 +199,6 @@ function main() {
             intento = ingresoErrado(esquivarDanioAcumulado, danioAcumulado, intento);
 
         }
-    } while (llave != LLAVE_FIN && intento <= MAX_INTENTOS && todosMuertos != true);
+    } while (llave != LLAVE_FINAL && intento <= MAX_INTENTOS && todosMuertos != true);
 }
 main();
